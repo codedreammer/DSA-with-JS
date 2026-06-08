86e2d1072ea71e4e3b36899615b8ae7da3fdd491
Akshay Mehta <nkanal38@gmail.com>
2026-06-08 21:16:06 +0530

    learning to build logic on leetcode problem

--- JS concepts/function/Q 242.js
+++ JS concepts/function/Q 242.js
@@ -20,8 +20,11 @@
 
     let freq1 = {};
     let freq2 = {};
-
-    for (let  key in freq1) {
+    
+    if(s.length !== t.length){
+        console.log("not");
+        
+    }
     for (let i = 0; i < s.length; i++) {
         let num = s[i];
 
@@ -31,17 +34,24 @@
             freq1[num] = 1
         }
     }
+
+    for (let i = 0; i < t.length; i++) {
+        let num = t[i];
+
+        if (freq2[num]) {
+            freq2[num]++
+        }else{
+            freq2[num] = 1
+        }
     }
-        console.log(freq1[key]);
+    console.log(freq1);
+    console.log(freq2);
 
-    // for (let i = 0; i < t.length; i++) {
-    //     let num = t[i];
-
-    //     if (freq2[num]) {
-    //         freq2[num]++
-    //     }else{
-    //         freq2[num] = 1
-    //     }
-    // }
-    // console.log(freq1);
-    // console.log(freq2);
+        for(let key in freq1){
+        if (freq1[key] === freq2[key]) {
+            console.log("anaram");
+            
+        }else{
+            console.log("Not Anagram");
+        }
+    }
