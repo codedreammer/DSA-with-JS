e55e591ffdbb587b34f4776586de43e047b46cac
Akshay Mehta <nkanal38@gmail.com>
2026-06-08 20:47:10 +0530

    learn how to find frequency

---
+++ JS concepts/function/Q 242.js
@@ -0,0 +1,47 @@
+    // var isAnagram = function(s, t) {
+    // if (s.length !== t.length) return false;
+
+    // const count = {};
+
+    // for (const ch of s) {
+    //     count[ch] = (count[ch] || 0) + 1;
+    // }
+
+    // for (const ch of t) {
+    //     if (!count[ch]) return false;
+    //     count[ch]--;
+    // }
+
+    // return true;
+    // };
+
+    let s = "anagram";
+    let t = "nagaram";
+
+    let freq1 = {};
+    let freq2 = {};
+
+    for (let  key in freq1) {
+    for (let i = 0; i < s.length; i++) {
+        let num = s[i];
+
+        if (freq1[num]) {
+            freq1[num]++
+        }else{
+            freq1[num] = 1
+        }
+    }
+    }
+        console.log(freq1[key]);
+
+    // for (let i = 0; i < t.length; i++) {
+    //     let num = t[i];
+
+    //     if (freq2[num]) {
+    //         freq2[num]++
+    //     }else{
+    //         freq2[num] = 1
+    //     }
+    // }
+    // console.log(freq1);
+    // console.log(freq2);
