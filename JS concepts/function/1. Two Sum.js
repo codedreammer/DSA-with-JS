c5a27fb5b6c4b53aa1d283884138fd4de4e2c6d2
Akshay Mehta <nkanal38@gmail.com>
2026-06-10 22:11:39 +0530

    solve 01 of leetcode

---
+++ JS concepts/function/1. Two Sum.js
@@ -0,0 +1,14 @@
+let arr = [2,7,11,15];
+let target = 9;
+
+function sum(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return[i, j];
+            }
+        }
+    }
+    return null
+}
+console.log(sum(arr, target));
