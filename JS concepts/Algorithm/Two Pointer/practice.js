4ee3025dd40d96e68c3ca33f1bde5a410e231c9b
Akshay Mehta <nkanal38@gmail.com>
2026-06-24 22:33:54 +0530

    solve 167 question number

--- JS concepts/Algorithm/Two Pointer/practice.js
+++ JS concepts/Algorithm/Two Pointer/practice.js
@@ -1,5 +1,6 @@
 // first two pointer question 
 
+/*
 arr = [2,3,4,7,11,15]
 target = 15
 
@@ -22,3 +23,35 @@ function twosum(arr, target) {
     }
 }
 console.log(twosum(arr, target));
+*/
+
+// second question 
+
+/*
+arr = [1,2,3,4,5,6,7,8]
+target = 9
+
+function twopointer(arr, target) {
+    
+    let left = 0;
+    let right = arr.length-1;
+    
+    while (left < right) {
+        let sum = arr[left] + arr[right]
+
+        if (sum === target) {
+            return [arr[left], arr[right]];
+    }
+    else if(sum < target){
+        left++;
+    }else{
+        right--;
+    }
+    }
+
+}
+console.log(twopointer(arr, target));
+*/
+
+
+
