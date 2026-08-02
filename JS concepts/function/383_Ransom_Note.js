ee2afe2b331a0f1bbdbf12427ffac50c1f504833
Akshay Mehta <nkanal38@gmail.com>
2026-08-02 21:55:46 +0530

    revison of hashing

--- JS concepts/function/383_Ransom_Note.js
+++ JS concepts/function/383_Ransom_Note.js
@@ -1,5 +1,5 @@
-let arr = "ransomNote";
-let arr1 = "magazine";
+// let arr = "ransomNote";
+// let arr1 = "magazine";
 
 function appared(arr, arr1) {
     let freq1 = {};
@@ -25,11 +25,53 @@ function appared(arr, arr1) {
             freq2[num] = 1
         }
     }
+    // for(let key in freq1){
+    //     if(freq1[key] > (freq2[key] || 0)){
+    //         return false;
+    //     }
+    // }
+    //OR
     for(let key in freq1){
-        if(freq1[key] > (freq2[key] || 0)){
+        if(freq1[key] !== freq2[key]){
             return false;
         }
     }
+
     return true;
 }
-console.log(appared(arr , arr1));
+// console.log(appared(arr , arr1));
+
+let arr3 = "abce"
+let arr4 = "abcd"
+
+function FindUnique(arr1,arr2){
+
+    let freq1 = {}
+    let freq2 = {}
+
+    for (let i = 0; i < arr3.length; i++) {    
+    }{
+        if(freq1[num]){
+            freq1[num]++
+        }
+        else{
+            freq1[num] = 1 
+        }
+    }
+    for (let i = 0; i < arr4.length; i++) {
+        if (freq2[num]) {
+            freq2[num]++
+        }
+        else{
+            freq2[num] = 1
+        }
+    }
+    
+    for(let key in freq1){
+        if(freq1[key] !== freq2[key]) {
+            return false;
+        }
+    }
+
+}
+console.log(appared(arr3,arr4));
